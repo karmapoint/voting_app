631d62f66c90699178622c747ff8a27735f451ce
Brent <karmapoint@gmail.com>
2017-08-19 19:55:10 -0700

    add upvoting capability

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,6 +1,39 @@
 class ProductList extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      products:[],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount(){
+    this.setState({ products: Seed.products });
+  }
+
+  handleProductUpVote(productId){
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+
+        // Object.assign clones the existing product
+        // the 3 args are the new object, the object whose products you want to
+        // copy, and then any changes you want to override
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
   render() {
-    const products = Seed.products.sort((a,b)=> (
+    const products = this.state.products.sort((a,b)=> (
       b.votes - a.votes
     ));
     const productComponents = products.map(product => (
@@ -13,6 +46,7 @@ class ProductList extends React.Component {
           votes={product.votes}
           submitterAvatarUrl={product.submitterAvatarUrl}
           productImageUrl={product.productImageUrl}
+          onVote={this.handleProductUpVote}
         />
       ));
     return (
@@ -24,6 +58,16 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+
+//when definining custom methods on React Component classes,
+// we need to bind this in the constructor so it will work
+  constructor(props){
+    super(props);
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+  handleUpVote(){
+    this.props.onVote(this.props.id);
+  }
   render() {
     return (
       <div className="item">
@@ -32,7 +76,7 @@ class Product extends React.Component {
         </div>
         <div className="middle aligned content">
           <div className="header">
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className="large caret up icon" />
             </a>
             {this.props.votes}
